b617fb6d5d656f47dc3f29b64656b5b606adb6e9
ishandasofficial <das.ishan71006@gmail.com>
2026-02-27 22:27:44 +0530

    27/02/26 Practice

---
+++ Grade_Decider.js
@@ -0,0 +1,15 @@
+Percentage = prompt("Enter your percentage ::");//Taking input 
+
+if(Percentage>90 && Percentage<=100){
+    console.log("Your grade is A ");
+}else if(Percentage>80 && Percentage<=90){
+    console.log("Your grade is B")
+}else if(Percentage>60 && Percentage<=80){
+    console.log("Your grade is C")
+}else if(Percentage>40 && Percentage<=60){
+    console.log("Your grade is D")
+}else if(Percentage>0 && Percentage<=40){
+    console.log("Your grade is F means you are fail")
+}
+
+console.log("Hare Krishna");
---
+++ Practice1.js
@@ -0,0 +1,16 @@
+
+const Product = {
+    title: "Ball Pen",
+    price: 10,
+    available: "Amazon"
+}
+console.log(Product.title,Product.price,Product.available);
+
+const Profile = {
+    name: "RadhaKrishna",
+    age: 25,
+    gmail: "radhakrishna@gmail.com",
+}
+console.log(Profile.name,Profile.age,Profile.gmail);
+
+console.log(1+2)
