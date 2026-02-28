1d436aa0b696b7fd8a92ea5e6da9780b4318f298
ishandasofficial <das.ishan71006@gmail.com>
2026-02-28 14:38:27 +0530

    28/02/26Practice

---
+++ Username_Generator.js
@@ -0,0 +1,8 @@
+let uname = prompt("Please enter your full name :: ");
+
+loweruname = uname.toLowerCase();
+
+realusername = "@" + loweruname + "genz";
+
+console.log("Your username is ::", realusername);
+
