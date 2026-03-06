194fd72970de43b800ff00476eda52da3bc57c5f
ishandasofficial <das.ishan71006@gmail.com>
2026-03-06 08:59:59 +0530

    06/03/2026

---
+++ Function_Demo.js
@@ -0,0 +1,10 @@
+function add(num1 , num2){
+    return num1 + num2 
+}
+
+let a = parseInt(prompt("Enter the first number"))
+let b = parseInt(prompt("Enter the second number"))
+
+let result = add(a,b)
+
+alert("The sum of the two numbers is " + result)
